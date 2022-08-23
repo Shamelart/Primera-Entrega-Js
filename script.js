3b78c8c3e15cea1d2c5af08198f24c095f718828
primeraEntregaJs <shamelart.ca@gmail.com>
2022-08-23 08:51:35 -0300

    Primera Entrega Js

---
+++ script.js
@@ -0,0 +1,23 @@
+//Calcular el factorial de un número introducido por el usiario
+//Utilizando estructura do...while
+let factorial=1;
+let num=parseInt(prompt("Ingresa un número:"));
+let a=num;
+let i=num
+do {factorial=factorial*i; i--;
+}
+while(i>0)
+    alert("El factorial del número " + a + " es: " + factorial);
+
+
+//El mismo ejecicio
+//Utilizando estructura for
+/*let num=parseInt(prompt("Ingresa un número:"));
+let a=num;
+let factorial=1;
+for (let i=num; i>0; i--){
+    factorial=factorial*i;
+}
+alert("El factorial del número " + a + " es: " + factorial);*/
+
+
